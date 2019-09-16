e9de0ac2a68478be74ca08716f6d024a68f94e34
Mateja Petrovic <ma.petrovic@levi9.com>
2019-09-17 01:34:05 +0200

    fix: handles empty

--- cyclic-rotation.ts
+++ cyclic-rotation.ts
@@ -2,8 +2,10 @@ const rotate = (a: number[]) => a.slice(-1).concat(a.slice(0, -1));
 
 function solution(A: number[], K: number) {
   const k = K % A.length;
-  return Array(k)
-    .fill(null)
-    .map((_, i) => i)
-    .reduce(rotate, A);
+  return A.length
+    ? Array(k)
+        .fill(null)
+        .map((_, i) => i)
+        .reduce(rotate, A)
+    : [];
 }
